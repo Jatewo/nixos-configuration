f583aa2c600fb291ec272527db818f4e296e7cf3
Jatewo <jacobterpewoods@gmail.com>
2025-07-30 21:13:50 +0200

    Updated labeling to use flake git rev

--- modules/labeling.nix
+++ modules/labeling.nix
@@ -1,8 +1,15 @@
+
+
 { config, pkgs, lib, ... }:
 
 let
-  # A helper to get the Git commit short hash from the flake
-  gitRevision = lib.substring 0 7 (config.flake.rev or "dirty");
+  # Explicitly get the current flake, which is guaranteed to be available
+  # when building with 'nixos-rebuild --flake'.
+  self = builtins.getFlake "path:/etc/nixos";
+
+  # Get the git revision from the flake. If it's not a git repo,
+  # it will be 'null'. The `or` fallback provides a default value.
+  gitRevision = lib.substring 0 7 (self.rev or self.outPath or "no-rev");
 in
 {
   options.system.nixos.labelDescription = lib.mkOption {
@@ -11,8 +18,8 @@ in
     description = "A short description to add to the system label.";
   };
 
-  # Put your configuration attributes inside the `config` attribute
   config = {
+    # Replace spaces with hyphens for a valid label string
     system.nixos.label = "Desktop-${gitRevision}-${builtins.replaceStrings [" "] ["-"] config.system.nixos.labelDescription}";
   };
 }

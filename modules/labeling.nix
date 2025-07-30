35b6821ba1a5bcad0ce913b5b79d1a34f1b0ec53
Jatewo <jacobterpewoods@gmail.com>
2025-07-30 20:59:24 +0200

    Added labeling functionality (WIP)

--- modules/labeling.nix
+++ modules/labeling.nix
@@ -1,26 +1,18 @@
-{ config, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  Get the current date and time
-  # This uses a dummy value during builds to ensure reproducibility
-  # and will only use the actual time when the flake is evaluated
-  currentDateTime = builtins.currentTime;
-  formattedTime = builtins.substring 0 16 (builtins.split "T" currentDateTime);
-
-  # A short description can be passed via an option
-  shortDescription = config.system.nixos.labelDescription or "";
-
-  # Construct the final label
-  finalLabel = "Desktop - ${formattedTime} ${shortDescription}";
+  # A helper to get the Git commit short hash from the flake
+  gitRevision = lib.substring 0 7 (config.flake.rev or "dirty");
 in
 {
-  # This option is what you will set in your host config
   options.system.nixos.labelDescription = lib.mkOption {
     type = lib.types.str;
     default = "";
     description = "A short description to add to the system label.";
   };
 
-  # This sets the actual system label using the constructed string
-  system.nixos.label = finalLabel;
+  # Put your configuration attributes inside the `config` attribute
+  config = {
+    system.nixos.label = "Desktop-${gitRevision}-${builtins.replaceStrings [" "] ["-"] config.system.nixos.labelDescription}";
+  };
 }

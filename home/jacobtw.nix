369f7b34ea4ed7497cea7052d85e8636b6cc20b4
Jatewo <jacobterpewoods@gmail.com>
2025-07-31 12:53:28 +0200

    Added config for neovim

--- home/jacobtw.nix
+++ home/jacobtw.nix
@@ -13,7 +13,32 @@
   ];
 
   xdg.configFile."nvim/init.lua".text = ''
-  -- your init.lua contents here
+  -- Bootstrap lazy.nvim plugin manager
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+      vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+      })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
+    require("lazy").setup({
+      {
+        "zbirenbaum/copilot.lua",
+        event = "InsertEnter",
+        config = function()
+          require("copilot").setup({
+            suggestion = { enabled = true, auto_trigger = true },
+            panel = { enabled = false },
+          })
+        end,
+      },
+    })
   '';
 
   programs.neovim = {

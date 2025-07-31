662c00dc0b9589a34ca7675512bbab780ee447b8
Jatewo <jacobterpewoods@gmail.com>
2025-07-31 12:31:14 +0200

    Added home file for user jacobtw

---
+++ home/jacobtw.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "jacobtw";
+  home.homeDirectory = "/home/jacobtw";
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    kate
+    git
+    neofetch
+  ]
+
+  programs.neovim = {
+    enable = true;
+    withNodeJs = true;
+    viAlias = true;
+    vimAlias = true;
+    extraPackages = with pkgs; [ gcc python3 nodejs ];
+  };
+
+  # Optional
+  programs.git.enable = true;
+  programs.zsh.enable = true;
+
+  home.stateVersion = "24.05"; # Match your system
+}

b08442dbba85bba3a4bc54920a5f7828ff6346a9
Jatewo <jacobterpewoods@gmail.com>
2025-10-04 15:19:14 +0200

    Added nvidia hardware config and 32 bit to desktop host

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -12,9 +12,24 @@
     ../modules/development.nix
   ];
 
+  boot.kernelParams = [ "nvidia-drm.modeset=1" ]; # Enable DRM kernel mode setting for NVIDIA
+  services.xserver.videoDrivers = [ "nvidia" ]; # Use the NVIDIA driver
+
+  hardware.nvidia = {
+    modesetting.enable = true; # Enable DRM kernel mode setting
+    powerManagement.enable = false; # Enable NVIDIA power management
+    open = true; # Enable NVIDIA Open GPU kernel module
+    nvidiaSettings = true; # Enable nvidia-settings utility
+    package = config.boot.kernelPackages.nvidiaPackages.stable; # Use the stable NVIDIA driver package
+  }; 
+
+  hardware.graphics.enable32Bit = true; # Enable 32-bit support for graphics drivers
+  hardware.steam-hardware.enable = true; # Enable 32-bit support for Steam and games
+  
+
   nix.gc = {
-    automatic = true;
-    dates = "daily";
+    automatic = true; # Enable automatic garbage collection
+    dates = "daily"; # Run garbage collection daily
     options = "--delete-older-than 30d"; # remove store paths not used in last 30 days
   };
 

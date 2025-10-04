b4f103fb23b17436db91f21b3796c0df008b1589
Jatewo <jacobterpewoods@gmail.com>
2025-10-04 15:17:48 +0200

    Added useDHCP to networking

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 
 {
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -12,6 +13,14 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+	
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp9s0.useDHCP = lib.mkDefault true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];

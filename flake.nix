6f077022bc6605ac1104d3909e0562282b5a4df0
Jatewo <jacobterpewoods@gmail.com>
2026-04-02 16:00:16 +0200

    Moved secure boot logic into module

--- flake.nix
+++ flake.nix
@@ -3,68 +3,43 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, lanzaboote, ... }@inputs:
-    let
-      # Define a common function to import nixpkgs and set config
-      importPkgs = system: import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
+  outputs = { self, nixpkgs, home-manager, lanzaboote, ... } @ inputs: {
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/desktop.nix
+
+          ({ config, pkgs, ... }: {
+            nixpkgs.config.allowUnfree = true;
+            nixpkgs.config.permittedInsecurePackages = [
+              "libsoup-2.74.3"
+            ];
+          })
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jacobtw = import ./home/jacobtw.nix;
+          }
+
+          lanzaboote.nixosModules.lanzaboote
+        ];
       };
-
-      # Define the systems you want to support
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-
-      # A helper to apply a function to each supported system
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
-    in {
-      # This is the key change. Define nixosConfigurations at the top level.
-      nixosConfigurations = {
-        desktop = nixpkgs.lib.nixosSystem {
-	  specialArgs = { inherit inputs; };
-          system = "x86_64-linux"; # You must specify the system here
-          modules = [
-            ./hosts/desktop.nix
-            # You can also define your packages here using a single pkgs import
-            ({ config, pkgs, ... }: {
-              nixpkgs.config.allowUnfree = true;
-	      nixpkgs.config.permittedInsecurePackages = [
-	        "libsoup-2.74.3"
-	      ];
-            })
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.users.jacobtw = import ./home/jacobtw.nix;
-            }
-	    inputs.lanzaboote.nixosModules.lanzaboote
-          ];
-        };
-        # You could add other hosts here, e.g., 'laptop'
-        # laptop = nixpkgs.lib.nixosSystem { ... };
-      };
-
-      # You can still use a helper like flake-utils to define packages for other systems
-      # This is useful for `nix build` or `nix shell` commands
-      packages = forAllSystems (system:
-        let pkgs = importPkgs system; in {
-          # Define packages here, e.g.,
-          # my-package = pkgs.hello;
-        });
-
-      # You can also add dev shells
-      devShells = forAllSystems (system:
-        let pkgs = importPkgs system; in {
-          # my-dev-shell = pkgs.mkShell { ... };
-        });
     };
+  };
 }
---
+++ modules/secure-boot.nix
@@ -0,0 +1,11 @@
+{ pkgs, lib, ... }:
+{
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+  };
+
+  environment.systemPackages = [ pkgs.sbctl ];
+}

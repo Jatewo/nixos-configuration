2ae57ba4d05afc9f4c96dd5c26dfe583873d1ab4
Jatewo <jacobterpewoods@gmail.com>
2026-04-02 16:24:42 +0200

    Added nvim-tree to neovim

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -79,3 +79,26 @@
     })
   '';
 }
+
+{
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+          "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+          require("nvim-tree").setup({
+            sort_by = "case_sensitive",
+            view = {
+              width = 30,
+            },
+            renderer = {
+              group_empty = true,
+            },
+            filters = {
+              dotfiles = false,
+            },
+          })
+          
+          vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+        end,
+      },

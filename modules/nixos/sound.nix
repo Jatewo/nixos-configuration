726c2e30baa8cdbe480d6a3ac514d7070c0af37c
Jatewo <jacobterpewoods@gmail.com>
2026-04-07 19:48:27 +0200

    Headset power issue fix

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -4,11 +4,20 @@
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="1038", ATTRS{idProduct}=="1290", TEST=="power/control", ATTR{power/control}="on"
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="1038", ATTRS{idProduct}=="1294", TEST=="power/control", ATTR{power/control}="on"
+  '';
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig.pipewire-pulse."10-disable-suspend" = {
+      "pulse.cmd" = [
+        { cmd = "load-module"; args = "module-suspend-on-idle"; flags = ["nofail"]; }
+      ];
+    };
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 

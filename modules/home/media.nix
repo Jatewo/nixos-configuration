bfb5ba3e0fa7de6d4701cdfcde036aa3d80ca773
Jatewo <jacobterpewoods@gmail.com>
2026-04-09 15:00:33 +0200

    Proper naming of pear-desktop

--- modules/home/media.nix
+++ modules/home/media.nix
@@ -4,4 +4,12 @@
   home.packages = with pkgs; [
     pear-desktop
   ];
+
+  xdg.desktopEntries.pear-desktop = {
+    name = "Pear Desktop";
+    exec = "env PULSE_PROP=\"application.name='Pear Desktop' media.role=music\" pear-desktop %U";
+    icon = "pear-desktop";
+    terminal = false;
+    categories = [ "Audio" "Music" "Player" ];
+  };
 }

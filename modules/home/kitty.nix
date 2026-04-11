4e3a2b5889012ae1d5ce1dd989147162d9cffabd
Jatewo <jacobterpewoods@gmail.com>
2026-04-12 01:21:17 +0200

    Added kitty terminal emulator

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -8,7 +8,10 @@
       size = 11;
     };
     settings = {
-      background_opacity = "0.95";
+      background_opacity = "0.8";
+      dynamic_background_opacity = true;
+      window_padding_width = 5;
+      background_blur = 5;
       enable_audio_bell = false;
       confirm_os_window_close = 0;
     };

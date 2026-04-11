4ce69b9995feb0b30775633c478a9244f0a2ba7f
Jatewo <jacobterpewoods@gmail.com>
2026-04-11 18:21:34 +0200

    Renamed neovim to nixvim and converted to nixvim syntax

---
+++ modules/home/nixvim.nix
@@ -0,0 +1,126 @@
+{ config, pkgs, ... }:
+
+{
+  programs.nixvim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    withNodeJs = true;
+
+    extraPackages = with pkgs; [ gcc python3 nodejs ];
+
+    opts = {
+      clipboard = "unnamedplus";
+    };
+
+    colorschemes.tokyonight.enable = true;
+
+    plugins = {
+      copilot-lua = {
+        enable = true;
+        panel = {
+          enabled = false;
+        };
+        suggestion = {
+          enabled = true;
+          autoTrigger = true;
+          keymap = {
+            accept = "<Tab>";
+            acceptWord = false;
+            acceptLine = false;
+            next = "<C-k>";
+            prev = "<C-j>";
+          };
+        };
+      };
+
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+          ensure_installed = [
+            "c"
+            "cpp"
+            "python"
+            "lua"
+            "javascript"
+          ];
+        };
+      };
+
+      web-devicons = {
+        enable = true;
+        settings = {
+          override = {
+            nix = {
+              icon = "❄";
+              color = "#7ea3cb";
+              name = "Nix";
+            };
+            md = {
+              icon = "📝";
+              color = "#519aba";
+              name = "Markdown";
+            };
+          };
+        };
+      };
+
+      nvim-tree = {
+        enable = true;
+	settings = {
+	  sort_by = "case_sensitive";
+	  view = {
+            width = 30;
+          };
+          filters = {
+            dotfiles = false;
+          };
+          renderer = {
+            group_empty = true;
+            icons = {
+              glyphs = {
+                folder = {
+                  arrow_closed = "▶";
+                  arrow_open = "▼";
+                  default = "📁";
+                  open = "📂";
+                  empty = "🗀";
+                  empty_open = "🗁";
+                  symlink = "🔗";
+                  symlink_open = "🔗";
+                };
+	        git = {
+                  unstaged = "✗";
+                  staged = "✓";
+                  untracked = "★";
+                  deleted = "⊖";
+                  ignored = "◌";
+	        };
+              };
+            };
+          };
+        };
+      };
+      fugitive.enable = true;
+    };
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<C-n>";
+        action = "<cmd>NvimTreeToggle<CR>";
+        options = {
+          silent = true;
+          noremap = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>gs";
+        action = "<cmd>Git<CR>";
+      }
+    ];
+  };
+}

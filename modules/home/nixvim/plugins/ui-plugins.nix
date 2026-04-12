a3dddc5633dfba2c139b9a8a1b2b87b54ea18c6c
Jatewo <jacobterpewoods@gmail.com>
2026-04-12 02:18:56 +0200

    Fix: added missing plugins wrapper for web-devicons

--- modules/home/nixvim/plugins/ui-plugins.nix
+++ modules/home/nixvim/plugins/ui-plugins.nix
@@ -1,40 +1,42 @@
 {
-  web-devicons = {
-    enable = true;
-    settings = {
+  plugins = {
+    web-devicons = {
+      enable = true;
+      settings = {
+      };
     };
-  };
 
-  plugins.nvim-tree = {
-    enable = true;
-    settings = {
-      sort_by = "case_sensitive";
-      view = {
-        width = 30;
-      };
-      filters = {
-        dotfiles = false;
-      };
-      renderer = {
-        group_empty = true;
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "▶";
-              arrow_open = "▼";
-              default = "📁";
-              open = "📂";
-              empty = "🗀";
-              empty_open = "🗁";
-              symlink = "🔗";
-              symlink_open = "🔗";
-            };
-            git = {
-              unstaged = "✗";
-              staged = "✓";
-              untracked = "★";
-              deleted = "⊖";
-              ignored = "◌";
+    nvim-tree = {
+      enable = true;
+      settings = {
+        sort_by = "case_sensitive";
+        view = {
+          width = 30;
+        };
+        filters = {
+          dotfiles = false;
+        };
+        renderer = {
+          group_empty = true;
+          icons = {
+            glyphs = {
+              folder = {
+                arrow_closed = "▶";
+                arrow_open = "▼";
+                default = "📁";
+                open = "📂";
+                empty = "🗀";
+                empty_open = "🗁";
+                symlink = "🔗";
+                symlink_open = "🔗";
+              };
+              git = {
+                unstaged = "✗";
+                staged = "✓";
+                untracked = "★";
+                deleted = "⊖";
+                ignored = "◌";
+              };
             };
           };
         };

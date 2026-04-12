38463c25b3d82bf070d3aed33a0fa29a3d7f96ac
Jatewo <jacobterpewoods@gmail.com>
2026-04-13 00:50:55 +0200

    Switched from nvim-tree to neo-tree

---
+++ modules/home/nixvim/plugins/neo-tree.nix
@@ -0,0 +1,15 @@
+{
+  plugins.neo-tree = {
+    enable = true;
+    settings = {
+      closeIfLastWindow = true;
+
+      window.mappings = {
+        # Use Space/Enter to open, or 'v'/'s' for splits
+        "<space>" = "open";
+        "v" = "open_vsplit";
+        "s" = "open_split";
+      };
+    };
+  };
+}
--- modules/home/nixvim/plugins/ui-plugins.nix
+++ modules/home/nixvim/plugins/ui-plugins.nix
@@ -6,41 +6,9 @@
       };
     };
 
-    nvim-tree = {
+    alpha = {
       enable = true;
-      settings = {
-        sort_by = "case_sensitive";
-        view = {
-          width = 30;
-        };
-        filters = {
-          dotfiles = false;
-        };
-        renderer = {
-          group_empty = true;
-          icons = {
-            glyphs = {
-              folder = {
-                arrow_closed = "▶";
-                arrow_open = "▼";
-                default = "📁";
-                open = "📂";
-                empty = "🗀";
-                empty_open = "🗁";
-                symlink = "🔗";
-                symlink_open = "🔗";
-              };
-              git = {
-                unstaged = "✗";
-                staged = "✓";
-                untracked = "★";
-                deleted = "⊖";
-                ignored = "◌";
-              };
-            };
-          };
-        };
-      };
+      theme = "dashboard";
     };
   };
 }

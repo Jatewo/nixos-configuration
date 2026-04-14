232f9e5c52f399e4507522b143c933b2048e1867
Jatewo <jacobterpewoods@gmail.com>
2026-04-14 19:25:09 +0200

    Configured styled tab manager for kitty

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,12 +1,11 @@
-{ pkgs, ...}:
-
-{
+{...}: {
   programs.kitty = {
     enable = true;
     font = {
       name = "JetBrainsMono Nerd Font";
       size = 11;
     };
+
     settings = {
       background_opacity = "0.8";
       dynamic_background_opacity = true;
@@ -14,6 +13,20 @@
       background_blur = 5;
       enable_audio_bell = false;
       confirm_os_window_close = 0;
+      tab_bar_style = "custom";
+      tab_bar_edge = "top";
+      tab_bar_margin_width = 10;
+      tab_bar_margin_height = "10 10";
+
+      active_tab_foreground = "#181825";
+      active_tab_background = "#c6a0f6";
+      active_tab_font_style = "bold";
+
+      inactive_tab_foreground = "#cad3f5";
+      inactive_tab_background = "#1e1e2e";
+      inactive_tab_font_style = "normal";
     };
   };
+
+  xdg.configFile."kitty/tab_bar.py".source = ./tab_bar_test.py;
 }

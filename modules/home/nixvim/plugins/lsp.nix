408b5afe8316cb4119b8aa17c1848ace2f9c08b8
Jatewo <jacobterpewoods@gmail.com>
2026-04-17 01:24:22 +0200

    Added more lsp specific keymaps to lspBuf

--- modules/home/nixvim/plugins/lsp.nix
+++ modules/home/nixvim/plugins/lsp.nix
@@ -12,14 +12,48 @@
       clangd.enable = true;
     };
 
-    keymaps.lspBuf = {
-      "gd" = "definition";
-      "gD" = "references";
-      "gt" = "type_definition";
-      "gi" = "implementation";
-      "K" = "hover";
-      "<leader>rn" = "rename";
-      "<leader>ca" = "code_action";
+    keymaps = {
+      silent = true;
+      lspBuf = {
+        "K" = {
+          action = "hover";
+          desc = "Hover Documentation";
+        };
+        "gd" = {
+          action = "definition";
+          desc = "Go to Definition";
+        };
+        "gr" = {
+          action = "references";
+          desc = "Go to References";
+        };
+        "gt" = {
+          action = "type_definition";
+          desc = "Go to Type Definition";
+        };
+        "gi" = {
+          action = "implementation";
+          desc = "Go to Implementation";
+        };
+        "<leader>rn" = {
+          action = "rename";
+          desc = "Rename Variable";
+        };
+        "<leader>ca" = {
+          action = "code_action";
+          desc = "Code Actions";
+        };
+      };
+      diagnostic = {
+        "[d" = {
+          action = "goto_prev";
+          desc = "Previous Diagnostic";
+        };
+        "]d" = {
+          action = "goto_next";
+          desc = "Next Diagnostic";
+        };
+      };
     };
   };
 }

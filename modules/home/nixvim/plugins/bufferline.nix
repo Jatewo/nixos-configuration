d138252f75dc1451a1899ab3bb866445e60cad68
Jatewo <jacobterpewoods@gmail.com>
2026-04-19 01:22:48 +0200

    Cleaned up config for bufferline

--- modules/home/nixvim/plugins/bufferline.nix
+++ modules/home/nixvim/plugins/bufferline.nix
@@ -1,41 +1,43 @@
 {
-  plugins.bufferline = {
-    enable = true;
-    settings = {
-      options = {
-        # Shows the LSP squigglies directly in the tabs
-        diagnostics = "nvim_lsp";
+  plugins = {
+    bufferline = {
+      enable = true;
+      settings = {
+        options = {
+          # Shows the LSP squigglies directly in the tabs
+          diagnostics = "nvim_lsp";
 
-        # Always show the tab bar, even if only one file is open
-        always_show_bufferline = true;
+          # Always show the tab bar, even if only one file is open
+          always_show_bufferline = true;
 
-        # Style of the tab separators
-        separator_style = "thin"; # Options: "slant", "thick", "thin"
+          # Style of the tab separators
+          separator_style = "thin"; # Options: "slant", "thick", "thin"
 
-        close_command = {
-          __raw = "function(n) require('mini.bufremove').delete(n, false) end";
+          close_command = {
+            __raw = "function(n) require('mini.bufremove').delete(n, false) end";
+          };
+
+          right_mouse_command = {
+            __raw = "function(n) require('mini.bufremove').delete(n, false) end";
+          };
+
+          offsets = [
+            {
+              filetype = "neo-tree";
+              text = "File Explorer";
+              text_align = "left";
+              separator = true;
+            }
+          ];
         };
-
-        right_mouse_command = {
-          __raw = "function(n) require('mini.bufremove').delete(n, false) end";
-        };
-
-        offsets = [
-          {
-            filetype = "neo-tree";
-            text = "File Explorer";
-            text_align = "left";
-            separator = true;
-          }
-        ];
       };
     };
-  };
 
-  plugins.mini = {
-    enable = true;
-    modules = {
-      bufremove = {};
+    mini = {
+      enable = true;
+      modules = {
+        bufremove = {};
+      };
     };
   };
 }

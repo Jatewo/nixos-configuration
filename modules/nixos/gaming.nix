fb5f107577e15eb16338047427fd8c666c173249
Jatewo <jacobterpewoods@gmail.com>
2026-04-20 22:32:33 +0200

    Added steam-hardware and allowunfree to gaming system module

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,10 +1,14 @@
 {pkgs, ...}: {
+  nixpkgs.config.allowUnfree = true;
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
   };
+
+  hardware.steam-hardware.enable = true; # Enable 32-bit support for Steam and games
 
   environment.systemPackages = with pkgs; [
     vulkan-tools # for vulkaninfo and other tools

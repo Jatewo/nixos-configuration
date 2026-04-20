f6400689f7279828367fe6c5b4776524cc447fbe
Jatewo <jacobterpewoods@gmail.com>
2026-04-20 22:32:03 +0200

    Added bluetooth system module

---
+++ modules/nixos/bluetooth.nix
@@ -0,0 +1,13 @@
+{...}: {
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        AutoEnable = true;
+        ControllerMode = "bredr";
+      };
+    };
+  };
+  services.blueman.enable = true;
+}
